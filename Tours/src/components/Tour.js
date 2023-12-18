b535e5a079291afc2d8e9b667ddb7035ab545fc5
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2023-12-18 22:44:23 +0200

    2. Tours completed

--- Tours/src/components/Tour.js
+++ Tours/src/components/Tour.js
@@ -8,6 +8,12 @@ const Tour = ({
   price,
   removeItem
 }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const changeShowMore = () => {
+    setShowMore(showMore => !showMore);
+  };
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -17,7 +23,7 @@ const Tour = ({
         <h4 className="tour-price">${price}</h4>
       </div>
 
-      <p>{info}</p>
+      <p>{showMore ? info : `${info.slice(0,200)}...`} <button onClick={changeShowMore}>{showMore ? 'Show less' : 'Show more'}</button></p>
       <button className="delete-btn" onClick={() => removeItem(id)}>Not interested</button>
       </footer>
     </article>
--- Tours/src/services/tourService.js
+++ Tours/src/services/tourService.js
@@ -4,6 +4,7 @@ export const getAllTours = async () => {
     try {
         const response = await fetch(url);
         const result = await response.json();
+        
         return result;
     } catch (error) {
         return error;
