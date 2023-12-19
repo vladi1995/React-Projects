7ba5c46c5fdfa69bd327eb17d398b84bc333f6e2
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2023-12-19 21:45:09 +0200

    Added functionality for no data left

--- Tours/src/App.js
+++ Tours/src/App.js
@@ -9,30 +9,40 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    tourService.getAllTours()
-      .then(tours => {
-        setTours(tours);
-        setLoading(false);
-      })
-      .catch(err => console.log(err));
+    fetchAllTours();
   }, []);
 
   const removeItem = (id) => {
     setTours(tours => tours.filter(tour => tour.id !== id));
   };
 
+  const fetchAllTours = () => {
+    tourService.getAllTours()
+      .then(tours => {
+        setTours(tours);
+        setLoading(false);
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <main>
-      {loading ? <Loading /> :
+      {tours.length === 0 ? <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={fetchAllTours}>Refresh</button>
+        </div>
+      </main> : 
+      loading ? <Loading /> :
         <>
           <div className="title">
             <h2>Our Tours</h2>
             <div className="underline"></div>
           </div>
-
+          
           <Tours tours={tours} removeItem={removeItem} />
         </>
-      };
+      }
     </main>
   );
 };
