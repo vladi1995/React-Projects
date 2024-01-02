b2f0bf2f547f1a329d32bf06513bce5a071baa57
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2024-01-02 14:31:15 +0200

    3. Reviews completed.

--- Reviews/src/Review.js
+++ Reviews/src/Review.js
@@ -3,7 +3,49 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [user, setUser] = useState(people[0]);
+
+  const onUserIncrement = (id) => {
+    if (id === people.length) {
+      setUser(people[0]);
+    } else {
+      setUser(people[id]);
+    }
+  }
+
+  const onUserDecrement = (id) => {
+    if (id === 0) {
+      setUser(people[people.length - 1]);
+    } else {
+      setUser(people[id-1]);
+    }
+  }
+
+  const onUserRandom = () => {
+    const min = 0;
+    const max = 4;
+    const randomNumber = Math.floor(Math.random() * (max-min) + min);
+    setUser(people[randomNumber]);
+  }
+
+  return (
+    <div className="review">
+      <div className="img-container">
+        <img src={user.image} className="person-img" />
+        <div className="quote-icon">
+          <FaQuoteRight />
+        </div>
+      </div>
+      <h4 className="author">{user.name}</h4>
+      <p className='job'>{user.job}</p>
+      <p className='info'>{user.text}</p>
+      <FaChevronLeft className='prev-btn' onClick={() => onUserDecrement(user.id - 1)} />
+      <FaChevronRight className='next-btn' onClick={() => onUserIncrement(user.id)} />
+      <div>
+        <button className='random-btn' onClick={() => onUserRandom()}>Surprise Me</button>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
