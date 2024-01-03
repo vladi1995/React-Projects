70c6d945a3f042ca78195db5d4c93c948bdcb4c4
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2024-01-03 10:42:04 +0200

    4. Accordion completed.

--- Accordion/src/App.js
+++ Accordion/src/App.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h3>Questions And Answers About Login</h3>
+        <div>
+          {data.map(question => <SingleQuestion key={question.id} {...question} />)}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- Accordion/src/Question.js
+++ Accordion/src/Question.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({
+  id,
+  title,
+  info
+}) => {
+  const [btnClicked, setBtnClicked] = useState(false);
+
+  const changeQuestion = () => {
+    setBtnClicked(() => !btnClicked);
+  }
+
+  return (
+    <div className="question">
+      <header>
+        <h4>{title}</h4>
+        {btnClicked ? <AiOutlineMinus className='btn' onClick={changeQuestion} /> : <AiOutlinePlus className='btn' onClick={changeQuestion} />}
+
+      </header>
+      {btnClicked && <p>{info}</p>}
+    </div>
+  );
 };
 
 export default Question;
