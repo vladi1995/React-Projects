bcde39af47c1c53a71dfc64c8d13b71b50680451
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2024-01-07 10:30:35 +0200

    11. Navbar completed

--- NavBar/src/Navbar.js
+++ NavBar/src/Navbar.js
@@ -4,17 +4,35 @@ import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(true);
+
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleShow = () => {
+    setShowLinks(oldShowLinks => !oldShowLinks);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect();
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight.height}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt="logo" />
-          <button className='nav-toggle'>
+          <button className='nav-toggle' onClick={toggleShow}>
             <FaBars />
           </button>
         </div>
-        <div className='links-container show-container'>
-          <ul className='links'>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
             {links.map(link => <li key={link.id}><a href={link.url}>{link.text}</a></li>)}
           </ul>
         </div>
